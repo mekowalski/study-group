aec1e5a3bff0f9a0535636d4d06c3ecc9e96a15b
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-24 13:03:06 -0700

    copy and add react components app from original DIR

---
+++ react-sg-022619/react-components-app2/src/CommentDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import faker from 'faker'
+
+const CommentDetail = () => {
+  return (
+    <div className='comment'>
+      <a href='/' className='avatar'>
+        <img alt='avatar' src={faker.image.avatar()}/>
+      </a>
+
+      <div className='content'>
+        <a href='/' className='author'>
+          Malind
+        </a>
+        <div className='metadata'>
+          <span className='date'>Today at 9:26AM</span>
+        </div>
+        <div className='text'>Nicely written article</div>
+      </div>
+    </div>
+  )
+}
+
+export default CommentDetail
